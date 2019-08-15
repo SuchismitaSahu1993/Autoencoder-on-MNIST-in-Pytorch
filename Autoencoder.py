ca29c584588e5449b0dce9167c278fab6fcf408e
Suchismita Sahu <suchismita.vssutb@gmail.com>
2019-08-15 11:33:11 -0700

    Create Autoencoder.py
    Code Updated

---
+++ Autoencoder.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 11:21:22 2019
+
+@author: suchismitasa
+"""
+
+import torch
+import torchvision as tv
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+# Data Preprocessing
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+trainTransform  = tv.transforms.Compose([tv.transforms.ToTensor(), tv.transforms.Normalize((0.1307,), (0.3081,))])
+trainset = tv.datasets.MNIST(root='./data',  train=True,download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False, num_workers=4)
+testset = tv.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+# Defining Model
+
+class Autoencoder(nn.Module):
+
+    def __init__(self):
+        super(Autoencoder,self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.Conv2d(6,16,kernel_size=5),
+            nn.ReLU(True))
+
+        self.decoder = nn.Sequential(             
+            nn.ConvTranspose2d(16,6,kernel_size=5),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(6,1,kernel_size=5),
+            nn.ReLU(True),
+            nn.Sigmoid())
+
+    def forward(self,x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+# Defining Parameters
+
+num_epochs = 5
+batch_size = 128
+model = Autoencoder().cpu()
+distance = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(),weight_decay=1e-5)
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = Variable(img).cpu()
+        # ===================forward=====================
+        output = model(img)
+        loss = distance(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.item()))
